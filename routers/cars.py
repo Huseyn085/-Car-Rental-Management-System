78ca7f8366b172236f29a38d94143b5ca2b0f221
Huseyn085 <huseeyn313@gmail.com>
2024-12-26 00:08:37 +0400

    Create cars.py

---
+++ routers/cars.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from car_rental import crud, models, schemas
+from car_rental.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("/", response_model=list[schemas.Car])
+def read_cars(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    cars = crud.get_cars(db, skip=skip, limit=limit)
+    return cars
+
+@router.get("/{car_id}", response_model=schemas.Car)
+def read_car(car_id: int, db: Session = Depends(get_db)):
+    db_car = crud.get_car(db, car_id=car_id)
+    if db_car is None:
+        raise HTTPException(status_code=404, detail="Car not found")
+    return db_car
+
+@router.post("/", response_model=schemas.Car)
+def create_car(car: schemas.CarCreate, db: Session = Depends(get_db)):
+    return crud.create_car(db=db, car=car)
