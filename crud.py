cf50b4620ff25d413dd94b0cf37280e03772105b
Huseyn085 <huseeyn313@gmail.com>
2024-12-26 00:32:22 +0400

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from car_rental import models, schemas
+import models, schemas
 
 def get_cars(db: Session, skip: int = 0, limit: int = 10):
     return db.query(models.Car).offset(skip).limit(limit).all()
