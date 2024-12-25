b6ac7306e9c815e309545425b32b19064c300edc
Huseyn085 <huseeyn313@gmail.com>
2024-12-26 00:34:06 +0400

    Update customers.py

--- routers/customers.py
+++ routers/customers.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from car_rental import crud, models, schemas
-from car_rental.database import SessionLocal, engine
+import crud, models, schemas
+from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -14,18 +14,18 @@ def get_db():
     finally:
         db.close()
 
-@router.get("/", response_model=list[schemas.Customer])
-def read_customers(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    customers = crud.get_customers(db, skip=skip, limit=limit)
-    return customers
+@router.get("/", response_model=list[schemas.Car])
+def read_cars(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    cars = crud.get_cars(db, skip=skip, limit=limit)
+    return cars
 
-@router.get("/{customer_id}", response_model=schemas.Customer)
-def read_customer(customer_id: int, db: Session = Depends(get_db)):
-    db_customer = crud.get_customer(db, customer_id=customer_id)
-    if db_customer is None:
-        raise HTTPException(status_code=404, detail="Customer not found")
-    return db_customer
+@router.get("/{car_id}", response_model=schemas.Car)
+def read_car(car_id: int, db: Session = Depends(get_db)):
+    db_car = crud.get_car(db, car_id=car_id)
+    if db_car is None:
+        raise HTTPException(status_code=404, detail="Car not found")
+    return db_car
 
-@router.post("/", response_model=schemas.Customer)
-def create_customer(customer: schemas.CustomerCreate, db: Session = Depends(get_db)):
-    return crud.create_customer(db=db, customer=customer)
+@router.post("/", response_model=schemas.Car)
+def create_car(car: schemas.CarCreate, db: Session = Depends(get_db)):
+    return crud.create_car(db=db, car=car)
